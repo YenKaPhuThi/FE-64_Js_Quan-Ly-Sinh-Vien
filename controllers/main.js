61e230426c2bacf7b44e5f9646c5479c188b1f65
Yen Chipu <yenkaphu96pnv@gmail.com>
2021-05-28 17:18:26 +0700

    [Js]: Create common functions

--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,25 @@
 var student = new Student();
 
-console.log(student);
+// Get element by id
+function getEleById(ele) {
+  return document.getElementById(ele);
+}
+
+// Get element by selector
+function getEleBySelector(ele) {
+  return document.querySelector(ele);
+}
+
+// Get element by selector all
+function getEleBySelectorAll(ele) {
+  return document.querySelectorAll(ele);
+}
+
+// Handle add new student
+function handleAddStudent() {
+  document.getElementById("addStudent").addEventListener("click", function () {
+    alert("say hi");
+  });
+}
+
+handleAddStudent();
